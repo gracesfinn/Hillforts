be564433292d75a4bd89df4a63b77e29220f3b44
Grace S Finnerty <50175328+gracesfinn@users.noreply.github.com>
2020-10-31 10:43:38 -0500

    SplashScreen Working

--- app/src/main/java/org/wit/hillforts/activities/SplashActivity.kt
+++ app/src/main/java/org/wit/hillforts/activities/SplashActivity.kt
@@ -1,25 +1,34 @@
 package org.wit.hillforts.activities
 
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.PersistableBundle
-import androidx.appcompat.app.AppCompatActivity
+import android.view.Window
+import android.view.WindowManager
 import org.wit.hillforts.R
 import org.wit.hillforts.main.MainApp
 
-class SplashActivity: AppCompatActivity(){
+class SplashActivity : AppCompatActivity() {
 
-    lateinit var app: MainApp
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //hiding title bar of this activity
+        window.requestFeature(Window.FEATURE_NO_TITLE)
+        //making this activity full screen
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        setContentView(R.layout.activity_splash)
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
-        setContentView(R.layout.splash_screen)
-        app = application as MainApp
-
+        //4second splash time
         Handler().postDelayed({
-            val intent = Intent( this@SplashActivity, HillfortListActivity:: class.java)
-            startActivity(intent)
-        }, 3000)
+            //start main activity
+            startActivity(Intent(this@SplashActivity, HillfortListActivity::class.java))
+            //finish this activity
+            finish()
+        }, 4000)
     }
 }
